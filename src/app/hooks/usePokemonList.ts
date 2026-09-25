27c308c38a237b5dcf6a6f76a65c7d6c7a4d69d0
Cristian Velasquez <crisdelmaso@gmail.com>
2026-09-25 09:28:42 -0500

    agrego hook lista pokemon

---
+++ src/app/hooks/usePokemonList.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import {
+  PokemonListItem,
+  PokemonListResponse,
+} from "../types/pokemon";
+
+export const usePokemonList = () => {
+  const [pokemon, setPokemon] = useState<PokemonListItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        setLoading(true);
+
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+        );
+
+        if (!response.ok) {
+          throw new Error("Error al obtener los Pokémon");
+        }
+
+        const data: PokemonListResponse =
+          await response.json();
+
+        setPokemon(data.results);
+      } catch (err) {
+        setError("No se pudieron cargar los Pokémon");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPokemon();
+  }, []);
+
+  return {
+    pokemon,
+    loading,
+    error,
+  };
+};
