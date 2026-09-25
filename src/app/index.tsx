682d6981bd823a5d5e8cb70277d6b8b9c451db38
Cristian Velasquez <crisdelmaso@gmail.com>
2026-09-25 09:15:07 -0500

    se agrega estilos base

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Pokedex</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Pokedex Lite
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
