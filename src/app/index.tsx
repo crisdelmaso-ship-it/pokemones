c1187e0d9db4a1f4330876198bc55197f1f4ef51
Cristian Velasquez <crisdelmaso@gmail.com>
2026-09-25 09:18:50 -0500

    feat: agrega diseño responsive

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,28 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, useWindowDimensions } from "react-native";
 
 export default function HomeScreen() {
+
+  const { width } = useWindowDimensions();
+
+  const columns =
+    width > 600
+      ? 3
+      : width > 380
+      ? 2
+      : 1;
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
         Pokedex Lite
+      </Text>
+
+      <Text>
+        Ancho: {width}
+      </Text>
+
+      <Text>
+        Columnas: {columns}
       </Text>
     </View>
   );
